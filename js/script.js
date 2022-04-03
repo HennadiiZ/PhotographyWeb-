e3cdd347b62f7e193a6f896f1f12a6bad8b5c73a
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-04-03 20:47:41 +0300

    THE MODAL w/ pics

--- js/script.js
+++ js/script.js
@@ -47,4 +47,34 @@ const form = document.getElementById("my-form");
             status.innerHTML = "Oops! There was a problem submitting your form"
         });
     }
-    form.addEventListener("submit", handleSubmit)
+    form.addEventListener("submit", handleSubmit);
+
+// =============================== THE MODAL =================================
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var myButton = document.getElementById("myButton");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+myButton.onclick = function(){
+  modal.style.display = "block";
+  // modalImg.src = this.src;
+  modalImg.src = modalImg.src;
+  captionText.innerHTML = modalImg.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+modal.onclick = function(e) {
+  if(e.target !== modalImg){
+    modal.style.display = "none";
+  }
+}
